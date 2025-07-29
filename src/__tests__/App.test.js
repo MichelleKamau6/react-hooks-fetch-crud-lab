916e5f586b32335cd3db55bfa9861cc37f98df6b
MichelleKamau6 <michwk06@gmail.com>
2025-07-29 11:22:04 +0300

    latest

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -6,7 +6,7 @@ import {
   screen,
   waitForElementToBeRemoved,
 } from "@testing-library/react";
-import "@testing-library/jest-dom";
+
 import { server } from "../mocks/server";
 import { resetData } from "../mocks/handlers";
 
